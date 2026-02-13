4bdc9bc232957a0a3a8f49bbaeb1c0abb1ef8165
UmerJamshiad <umerjamshaid481@gmail.com>
2026-02-13 18:55:18 +0500

    Now Starting the work on it

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'PipFa Tasks',
       routes: {
         /*'/intro_screen': (context) => const IntroScreen(),
         '/menu_screen': (context) => const MenuScreen(),
