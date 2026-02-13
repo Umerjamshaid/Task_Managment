be62cb99d7a8c03f2debf2e8a579b0eca4038819
UmerJamshiad <umerjamshaid481@gmail.com>
2026-02-13 19:30:29 +0500

    Added Profile photo

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('HOME SCREEN')));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            Container(
+              height: 45,
+              width: 45,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset('assets/images/avatar.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(child: Text('HOME SCREEN')),
+    );
   }
 }
